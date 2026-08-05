7eee95bb10f81c5f846af481e7ef4180c738b0b2
morgan <polardeveloper11@proton.me>
2026-08-05 15:28:00 -0300

    First java class

---
+++ src/main/java/br/com/etechas/ingressos/Pw2IngresosApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechas.ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2IngresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2IngresosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/etechas/ingressos/entity/Filme.java
@@ -0,0 +1,19 @@
+package br.com.etechas.ingressos.entity;
+import br.com.etechas.ingressos.enums.ClassificaçaoIndicativaEnum;
+import br.com.etechoracio.ingresso.enums.CategoriaFilmeEnum;
+import     br.com.etechoracio.ingresso.enums.SimNaoEnum;
+public class Filme {
+private Long id;
+    String nome;
+    Integer duracao;
+    ClassificaçaoIndicativaEnum classificao;
+    CategoriaFilmeEnum categoria;
+    Integer ano;
+    String capa;
+    String diretor;
+    String elenco;
+    String descricao;
+    double avaliacao;
+    SimNaoEnum emCartaz;
+
+}
