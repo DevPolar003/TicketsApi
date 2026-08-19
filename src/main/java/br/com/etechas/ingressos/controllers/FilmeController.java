a0ae998dfa200b1dd191890c4001908e146a4952
DevPolar003 <polardeveloper11@proton.me>
2026-08-19 14:16:39 -0300

    Add Controller method Post and GetById

--- src/main/java/br/com/etechas/ingressos/controllers/FilmeController.java
+++ src/main/java/br/com/etechas/ingressos/controllers/FilmeController.java
@@ -3,9 +3,7 @@ package br.com.etechas.ingressos.controllers;
 import br.com.etechas.ingressos.entity.Filme;
 import br.com.etechas.ingressos.enums.ClassificacaoIndicativaEnum;
 import br.com.etechas.ingressos.enums.SimNaoEnum;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,16 @@ public class FilmeController {
             ClassificacaoIndicativaEnum.LIVRE, SimNaoEnum.S),
         new Filme("O espeta cu,1L", 1L,
                 ClassificacaoIndicativaEnum.LIVRE, SimNaoEnum.S));
+    }
+
+    @GetMapping("/id")
+    public Filme buscarPorId(@PathVariable Long id){
+        return new Filme("Filme",id, ClassificacaoIndicativaEnum.A14, SimNaoEnum.N);
+    }
+
+    @PostMapping("/post")
+    public Filme cadastrarFilme(Filme filme){
+        filme.setId(500L);
+        return new Filme();
     }
 }
